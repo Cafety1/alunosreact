e707e8e966e0b103c4e6f0e7f3d6731ad165a6bf
vitor.bandeira <vitor.bandeira@animaeducacao.com.br>
2023-07-01 15:49:09 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,212 @@
+import React, { useState,useEffect } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios';
+import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
+import logoCadastro from './assets/cadastro.png'
+function App() {
+
+  const baseUrl="https://localhost:7249/alunos";
+
+  const [data, setData] = useState([]);
+  const [updateData, setUpdateData] = useState(true);
+  const pedidoGet = async() =>{
+    await axios.get(baseUrl)
+      .then(response => { 
+            setData(response.data);
+      }).catch(error =>{
+            console.log(error);
+       })
+  }
+
+  const pedidoPost = async() =>{
+    delete alunoSelecionado.id;
+    alunoSelecionado.idade =parseInt(alunoSelecionado.idade);
+      await axios.post(baseUrl, alunoSelecionado)
+      .then(response =>{
+        setData(data.concat(response.data));
+      }).catch(error =>{
+        console.log(error);
+      })
+      setUpdateData(true);
+
+  }
+
+  const pedidoPut= async() =>{
+    alunoSelecionado.idade = parseInt(alunoSelecionado.idade);
+    await axios.put(baseUrl+"/"+alunoSelecionado.id,alunoSelecionado)
+      .then(response =>{
+        var resposta = response.data;
+        var dadosAuxiliar = data;
+        dadosAuxiliar.map(aluno=>{
+          if(aluno.id===alunoSelecionado.id){
+            aluno.nome = resposta.nome;
+            aluno.email = resposta.email;
+            aluno.idade = resposta.idade;
+          }
+      });
+      abrirFecharModalEditar();
+      setUpdateData(true);
+      }).catch(error =>{
+        console.log(error);
+      })
+  }
+
+  const pedidoDelete = async() =>{
+    await axios.delete(baseUrl+"/"+alunoSelecionado.id)
+    .then(response =>{
+      setData(data.filter(aluno => aluno.id !== response.data));
+      abrirFecharModalExcluir();
+      setUpdateData(true);
+    }).catch(error =>{
+      console.log(error);
+    })
+
+  }
+
+  const [alunoSelecionado, setAlunoSelecionado] = useState({
+    id: '',
+    nome: '',
+    email:'',
+    idade:''
+  });
+  
+  const handleChange = e =>{
+    const{name,value} = e.target;
+    setAlunoSelecionado({
+      ...alunoSelecionado,[name]:value
+    });
+    console.log(alunoSelecionado);
+  }
+  const [modalIncluir, setModalIncluir] = useState(false);
+
+  const abrirFecharModalIncluir=()=>{
+    setModalIncluir(!modalIncluir);
+  }
+
+  const [modalEditar, setModalEditar] = useState(false);
+
+  const abrirFecharModalEditar=()=>{
+    setModalEditar(!modalEditar);
+  }
+  
+  const [modalExcluir, setModalExcluir] = useState(false);
+
+  const abrirFecharModalExcluir=()=>{
+    setModalExcluir(!modalExcluir);
+  }
+
+  const selecionarAluno=(aluno, opcao)=>{
+    setAlunoSelecionado(aluno);
+      (opcao==="Editar") ? 
+            abrirFecharModalEditar() : abrirFecharModalExcluir();
+  }
+
+
+
+  useEffect(()=>{
+    if(updateData){
+      pedidoGet();
+      setUpdateData(false);
+    }
+  },[updateData])
+
+  return (
+    <div className="aluno-container">
+      <br/>
+      <h3>Cadastro de Aluno</h3>
+      <header>
+                <button className = "btn btn-sucess" onClick={()=>abrirFecharModalIncluir()}>Incluir Novo Aluno</button>
+      </header> 
+      <table className = "table table-bordered">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Idade</th>
+            <th>Operação</th>
+          </tr>
+        </thead>
+        <tbody>
+            {data.map(aluno =>(
+              <tr key={aluno.id}>
+                <td>{aluno.id}</td>
+                <td>{aluno.nome}</td>
+                <td>{aluno.email}</td>
+                <td>{aluno.idade}</td>
+                <td>
+                  <button className="btn btn-primary" onClick={()=>selecionarAluno(aluno, "Editar")}>Editar</button> {" "}
+                  <button className="btn btn-danger" onClick={()=>selecionarAluno(aluno, "Excluir")}>Excluir</button>
+                </td>
+              </tr>
+            ))}
+
+        </tbody>
+      </table>
+      
+      <Modal isOpen ={modalIncluir}>
+        <ModalHeader> Incluir Aluno</ModalHeader>
+        <ModalBody>
+          <div className = "form-group">
+            <label>Nome:</label>
+            <br/>
+            <input type="text" className="form-control" name = "nome" onChange={handleChange}/>
+            <label>Email:</label>
+            <br/>
+            <input type="text" className="form-control" name = "email" onChange={handleChange}/>
+            <label>Idade:</label>
+            <br/>
+            <input type="text" className="form-control" name = "idade" onChange={handleChange}/>
+            <br/>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className = "btn btn-primary" onClick={()=>pedidoPost()}>Incluir</button>{" "}
+          <button className = "btn btn-danger" onClick={()=>abrirFecharModalIncluir()}>Cancelar</button>
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen ={modalEditar}>
+        <ModalHeader> Editar Aluno</ModalHeader>
+        <ModalBody>
+          <div className = "form-group">
+            <label>Id:</label>
+            <input type="text" className="form-control" readOnly value ={alunoSelecionado && alunoSelecionado.id}/>
+            <br />
+            <label>Nome:</label>
+            <br/>
+            <input type="text" className="form-control" name = "nome" onChange={handleChange}
+                                value={alunoSelecionado && alunoSelecionado.nome}/>
+            <label>Email:</label>
+            <br/>
+            <input type="text" className="form-control" name = "email" onChange={handleChange}
+                                value={alunoSelecionado && alunoSelecionado.email}/>
+            <label>Idade:</label>
+            <br/>
+            <input type="text" className="form-control" name = "idade" onChange={handleChange}
+                                value={alunoSelecionado && alunoSelecionado.idade}/>
+            <br/>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className = "btn btn-primary" onClick={()=>pedidoPut()}>Editar</button>{" "}
+          <button className = "btn btn-danger" onClick={()=>abrirFecharModalEditar()}>Cancelar</button>
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen={modalExcluir}>
+        <ModalBody>
+          Confirma a exclusão deste(a) aluno(a) : {alunoSelecionado && alunoSelecionado.nome} ?
+        </ModalBody>
+        <ModalFooter>
+          <button className ="btn btn-danger" onClick={()=>pedidoDelete()}>Sim</button>
+          <button className = "btn btn-secondary" onClick={()=>abrirFecharModalExcluir()}>Não</button>
+        </ModalFooter>
+      </Modal>
+
+    </div>
+  );
+}
+
+export default App;
